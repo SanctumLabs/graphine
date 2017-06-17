0fadae511a2fa8c26f345880352b90c69dc7b0be
Brian Lusina <lusinabrian@gmail.com>
2017-06-17 09:10:14 +0300

    add d3-tip

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
 import * as d3 from 'd3';
+import fetchData from '../api/api';
+
 
 export default class Chart extends Component{
     constructor(){
         super();
+        this.state ={
+            data : []
+        }
+    }
+
+    componentWillMount(){
+        let fastestTime = 2210;
+
+        fetchData().then(function(data){
+            let formattedData = data;
+            // format the data we get from api
+            formattedData.forEach((element)=>{
+                element.behind = element.Seconds - fastestTime;
+                // add the data to the legend
+                element.legend = element.Doping !== "" ? "Doping Allegations" : "No Doping allegations";
+            });
+            this.setState({data : formattedData});
+        }.bind(this)).catch((error)=>{
+            console.error("Error in Chart when fetching data:", error);
+        })
     }
 
     render(){

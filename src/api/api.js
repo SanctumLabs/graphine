5f3c72e9a1609f0bab2cf32035a7138fbe2e2c6c
Brian Lusina <lusinabrian@gmail.com>
2017-06-17 08:46:32 +0300

    create api and constants file

---
+++ src/api/api.js
@@ -0,0 +1,21 @@
+/**
+ * @author lusinabrian on 17/06/17.
+ * @notes: Handles api calls
+ */
+
+import * as constants from '../constants/constants';
+import axios from 'axios';
+
+/**
+ * Fetches data from url and returns response
+ * @param {String} url for fetching the data
+ * @returns {Promise.<TResult>} data array
+ * */
+export default function fetchData(url = constants.CYCLIST_DAT_URL){
+    return axios.get(url).then((response) => {
+        return response.data;
+    }).catch((error) =>{
+      // handle error
+        console.error("Error occurred when fetching data: ", error)
+    })
+}
---
+++ src/constants/constants.js
@@ -0,0 +1,6 @@
+/**
+ * @author lusinabrian on 17/06/17.
+ * @notes: Contants to be used in app
+ */
+export const CYCLIST_DAT_URL = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json";
+
